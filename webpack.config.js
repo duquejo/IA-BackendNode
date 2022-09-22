b9392dfc4663b70e86a621be13999cf00f8e950e
José Duque <duquejo01@gmail.com>
2022-09-21 20:59:35 -0500

    :construction: NOD-002: Serverless authorizer config

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
     plugins: [
         new ForkTsCheckerWebpackPlugin(),
         new ESLintPlugin({
-            extensions: ['tsx']
+            extensions: ['ts']
         })
     ],
     externals: [ nodeExternals() ],
